378bc1d58ad4c1b9741c05f6331851c6de3a2fa5
Youssef Soliman <youssefdevelops@gmail.com>
2020-01-22 16:56:10 -0700

    Implemented Drive class

---
+++ TeamCode/src/main/java/ca/webber/ftc/subsystems/Drive.java
@@ -0,0 +1,57 @@
+package ca.webber.ftc.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.Range;
+
+public final class Drive {
+    private DcMotorEx frontRight = null;
+    private DcMotorEx frontLeft = null;
+    private DcMotorEx backRight = null;
+    private DcMotorEx backLeft = null;
+    private boolean fastMode = true;
+    private int direction = 1;
+
+    public Drive(DcMotorEx frontRight, DcMotorEx frontLeft, DcMotorEx backRight, DcMotorEx backLeft) {
+        this.frontRight = frontRight;
+        this.frontLeft = frontLeft;
+        this.backRight = backRight;
+        this.backLeft = backLeft;
+    }
+
+    public void toggleFastMode() {
+        fastMode = !fastMode;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public void drive(double x, double y) {
+        double turn = -x;
+        double power = Math.hypot(x, y);
+        double bearing = Math.atan2(y, x) + Math.toRadians(45);
+        double driveY = (Math.sin(bearing)) * power * direction;
+        double driveX = (Math.cos(bearing)) * power * direction;
+
+        double frPower;
+        double flPower;
+        double brPower;
+        double blPower;
+
+        if (fastMode) {
+            frPower = Range.clip(Math.pow(turn + driveY, 3), -1.0, 1.0);
+            flPower = Range.clip(Math.pow(turn - driveX, 3), -1.0, 1.0);
+            brPower = Range.clip(Math.pow(turn + driveX, 3), -1.0, 1.0);
+            blPower = Range.clip(Math.pow(turn - driveY, 3), -1.0, 1.0);
+        } else {
+            frPower = Range.clip(Math.pow(turn + driveY, 3), -0.5, 0.5);
+            flPower = Range.clip(Math.pow(turn - driveX, 3), -0.5, 0.5);
+            brPower = Range.clip(Math.pow(turn + driveX, 3), -0.5, 0.5);
+            blPower = Range.clip(Math.pow(turn - driveY, 3), -0.5, 0.5);
+        }
+        frontRight.setPower(frPower);
+        frontLeft.setPower(flPower);
+        backRight.setPower(brPower);
+        backLeft.setPower(blPower);
+    }
+}
--- TeamCode/src/main/java/ca/webber/ftc/teleOp/Teleop.java
+++ TeamCode/src/main/java/ca/webber/ftc/teleOp/Teleop.java
@@ -5,15 +5,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-import java.lang.Math;
 
-import ca.webber.ftc.subsystems.FoundationMover;
-import ca.webber.ftc.subsystems.Lift;
+import ca.webber.ftc.subsystems.Drive;
 import ca.webber.ftc.subsystems.Intake;
+import ca.webber.ftc.subsystems.Lift;
 
 @TeleOp(name="Teleop", group="Iterative Opmode")
-
 public class Teleop extends OpMode
 {
     // Declare OpMode members.
@@ -28,8 +25,8 @@ public class Teleop extends OpMode
     private DcMotorEx rightIntake = null;
     //private DcMotorEx foundationMotor = null;
     //FoundationMover foundationMover;
-    Intake intake;
-    Lift lift;
+    private Intake intake;
+    private Lift lift;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -132,11 +129,7 @@ public class Teleop extends OpMode
     @Override
     public void loop() {
 
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double frPower;
-        double flPower;
-        double brPower;
-        double blPower;
+        Drive drive = new Drive(frontRight, frontLeft, backRight, backLeft);
 
         after = gamepad1.a;
 
@@ -147,46 +140,14 @@ public class Teleop extends OpMode
         before = after;
 
         if (gamepad1.x){
-            directionMultiplier = -1;
+            drive.setDirection(-1);
         }
 
         if (gamepad1.y){
-            directionMultiplier = 1;
+            drive.setDirection(1);
         }
-        // Left Stick for rotation, right for translation
-        // displacement vector described by magnitude and bearing
 
-        double turn = -gamepad1.left_stick_x;
-        double power = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
-
-        /* bearing is derived from gamepad x-y vector's angle
-        (unit circle layout), plus known robot orientation,
-        to produce absolute bearing angle, wrt to field */
-
-        double rightStickY = gamepad1.right_stick_y;
-        double rightStickX = gamepad1.right_stick_x;
-        double bearing = Math.atan2(rightStickY, rightStickX) + Math.toRadians(robotOrientation);
-        //unit circle layout, this var is in radians
-
-        double driveY = (Math.sin(bearing)) * power;
-        double driveX = directionMultiplier * (Math.cos(bearing)) * power;
-        //breaks vector into x-y components
-
-        telemetry.addData("Drive X", driveX);
-        telemetry.addData("Drive Y", driveY);
-
-        if(fastMode) {
-            frPower   = Range.clip(Math.pow(turn + driveY, 3), -1.0, 1.0) ;
-            flPower  = Range.clip(Math.pow(turn - driveX, 3), -1.0, 1.0) ;
-            brPower  = Range.clip(Math.pow(turn + driveX, 3), -1.0, 1.0) ;
-            blPower   = Range.clip(Math.pow(turn - driveY, 3), -1.0, 1.0) ;
-        }
-        else {
-            frPower   = Range.clip(Math.pow(turn + driveY, 3), -0.5, 0.5) ;
-            flPower  = Range.clip(Math.pow(turn - driveX, 3), -0.5, 0.5) ;
-            brPower  = Range.clip(Math.pow(turn + driveX, 3), -0.5, 0.5) ;
-            blPower   = Range.clip(Math.pow(turn - driveY, 3), -0.5, 0.5) ;
-        }
+        drive.drive(gamepad1.right_stick_x, gamepad1.right_stick_y);
 
         intake.shift(gamepad2.left_stick_x);
 
@@ -211,12 +172,6 @@ public class Teleop extends OpMode
 
         telemetry.addData("leftInt", leftIntake.getCurrentPosition());
         lift.move(gamepad2.left_stick_y);
-
-        // Send calculated power to wheels
-        frontRight.setPower(frPower);
-        frontLeft.setPower(flPower);
-        backRight.setPower(brPower);
-        backLeft.setPower(blPower);
 
         telemetry.addData("frontRight Drive", frontRight.getCurrentPosition());
         telemetry.addData("frontLeft Drive", frontLeft.getCurrentPosition());
