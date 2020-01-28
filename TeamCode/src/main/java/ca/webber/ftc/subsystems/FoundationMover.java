6f081225511f4a9c4615d16ba2598702db281ebe
Youssef Soliman <youssefdevelops@gmail.com>
2020-01-27 17:47:54 -0700

    Added foundation lock

--- TeamCode/src/main/java/ca/webber/ftc/subsystems/FoundationMover.java
+++ TeamCode/src/main/java/ca/webber/ftc/subsystems/FoundationMover.java
@@ -6,14 +6,25 @@ public class FoundationMover {
 
     private CRServo foundation1;
     private CRServo foundation2;
+    private boolean lock = false;
 
     public FoundationMover(CRServo foundation1, CRServo foundation2) {
         this.foundation1 = foundation1;
         this.foundation2 = foundation2;
     }
 
-    public void lockDown(double power) {
-        foundation1.setPower(power);
-        foundation2.setPower(power);
+    public void loop() {
+        if (lock) {
+            foundation1.setPower(1);
+            foundation2.setPower(-1);
+        }
+    }
+
+    public void toggleLock() {
+        lock = !lock;
+    }
+
+    public boolean getLock() {
+        return lock;
     }
 }
