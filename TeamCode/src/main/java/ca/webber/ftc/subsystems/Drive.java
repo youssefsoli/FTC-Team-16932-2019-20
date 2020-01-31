12d1a99b2091d541ed96b833673942c7cccd5315
Youssef Soliman <youssefdevelops@gmail.com>
2020-01-31 16:33:49 -0700

    Fixed Drive orientation

--- TeamCode/src/main/java/ca/webber/ftc/subsystems/Drive.java
+++ TeamCode/src/main/java/ca/webber/ftc/subsystems/Drive.java
@@ -5,10 +5,10 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.Range;
 
 public final class Drive {
-    private DcMotorEx frontRight = null;
-    private DcMotorEx frontLeft = null;
-    private DcMotorEx backRight = null;
-    private DcMotorEx backLeft = null;
+    private DcMotorEx frontRight;
+    private DcMotorEx frontLeft;
+    private DcMotorEx backRight;
+    private DcMotorEx backLeft;
     private boolean fastMode = true;
 
     public Drive(DcMotorEx frontRight, DcMotorEx frontLeft, DcMotorEx backRight, DcMotorEx backLeft) {
@@ -22,24 +22,21 @@ public final class Drive {
         fastMode = !fastMode;
     }
 
-    public boolean isFastMode() {
-        return fastMode;
-    }
-
     public void drive(Gamepad gamepad) {
         double x = gamepad.left_stick_x;
         double y = gamepad.left_stick_y;
 
         if (gamepad.dpad_up)
-            y = 1;
+            y = -1;
         else if (gamepad.dpad_right)
             x = 1;
         else if (gamepad.dpad_down)
-            y = -1;
+            y = 1;
         else if (gamepad.dpad_left)
             x = -1;
 
-        double turn = -gamepad.right_stick_x;
+        double turn = gamepad.right_stick_x;
+        double speedFactor = 0.3;
 
         double power = Math.pow(Math.hypot(x, y), 2);
         double bearing = Math.atan2(y, x) + Math.toRadians(45);
@@ -53,17 +50,16 @@ public final class Drive {
 
         turn *= 0.5;
 
-        if (fastMode) {
-            frPower = Range.scale(turn + driveY, -1.0, 1.0, -1.0, 1.0);
-            flPower = Range.scale(turn - driveX, -1.0, 1.0, -1.0, 1.0);
-            brPower = Range.scale(turn + driveX, -1.0, 1.0, -1.0, 1.0);
-            blPower = Range.scale(turn - driveY, -1.0, 1.0, -1.0, 1.0);
-        } else {
-            frPower = Range.scale(turn + driveY, -1.0, 1.0, -0.5, 0.5);
-            flPower = Range.scale(turn - driveX, -1.0, 1.0, -0.5, 0.5);
-            brPower = Range.scale(turn + driveX, -1.0, 1.0, -0.5, 0.5);
-            blPower = Range.scale(turn - driveY, -1.0, 1.0, -0.5, 0.5);
-        }
+        if (fastMode)
+            speedFactor = 0.5;
+
+        if (gamepad.left_stick_button)
+            speedFactor = 1;
+
+        frPower = Range.scale(turn + driveY, -1.0, 1.0, -speedFactor, speedFactor);
+        flPower = Range.scale(turn - driveX, -1.0, 1.0, -speedFactor, speedFactor);
+        brPower = Range.scale(turn + driveX, -1.0, 1.0, -speedFactor, speedFactor);
+        blPower = Range.scale(turn - driveY, -1.0, 1.0, -speedFactor, speedFactor);
 
         frontRight.setPower(frPower);
         frontLeft.setPower(flPower);
